446bc01f4c35624a5e40994efa30fb31256ea315
koji koyama <k.koyama37@gmail.com>
2021-08-08 22:53:42 +0900

    temp: sampleテストの実装。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}", internal_adder(2, 2))
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn my_test() {
+    assert_eq!(internal_adder(2, 2), 4);
 }
