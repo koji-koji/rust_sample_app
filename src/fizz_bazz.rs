23dd27dde9ca0d75eb47c37223df2a75b4642d8e
koji koyama <k.koyama37@gmail.com>
2021-08-10 01:00:47 +0900

    feature: fizz_buzzの別開放の作成。

--- src/fizz_bazz.rs
+++ src/fizz_bazz.rs
@@ -2,6 +2,9 @@ pub fn run() {
   for i in 1..100 {
     println!("{} {}", i, return_fizz_bazz(i));
   }
+  for i in 1..100 {
+    println!("{} {}", i, return_fizz_bazz2(i));
+  }
 }
 
 fn return_fizz_bazz(i: u32) -> &'static str {
@@ -16,6 +19,13 @@ fn return_fizz_bazz(i: u32) -> &'static str {
   };
 }
 
+fn return_fizz_bazz2(i: u32) -> std::string::String {
+  let fizz_result = if (i % 3) == 0 { "fizz" } else { "" };
+  let buzz_result = if (i % 5) == 0 { "buzz" } else { "" };
+  let answer = format!("{}{}", fizz_result, buzz_result);
+  answer
+}
+
 #[test]
 fn fizz_bazz_test() {
   assert_eq!(return_fizz_bazz(1), "");
@@ -23,4 +33,12 @@ fn fizz_bazz_test() {
   assert_eq!(return_fizz_bazz(4), "");
   assert_eq!(return_fizz_bazz(5), "buzz");
   assert_eq!(return_fizz_bazz(15), "fizzbuzz");
+}
+#[test]
+fn fizz_bazz_test2() {
+  assert_eq!(return_fizz_bazz2(1), "");
+  assert_eq!(return_fizz_bazz2(3), "fizz");
+  assert_eq!(return_fizz_bazz2(4), "");
+  assert_eq!(return_fizz_bazz2(5), "buzz");
+  assert_eq!(return_fizz_bazz2(15), "fizzbuzz");
 }
