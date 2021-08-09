3f51cffa7c7f2fef95d1cf141da40e1c53e4fded
koji koyama <k.koyama37@gmail.com>
2021-08-10 00:31:58 +0900

    refactor: 不要なreturn宣言の削除。

--- src/fizz_bazz.rs
+++ src/fizz_bazz.rs
@@ -1,26 +1,26 @@
 pub fn run() {
   for i in 1..100 {
-    println!("{} {}", i, print_fizz_bazz(i));
+    println!("{} {}", i, return_fizz_bazz(i));
   }
 }
 
-fn print_fizz_bazz(i: u32) -> &'static str {
-  if i % 15 == 0 {
-    return "fizzbuzz";
+fn return_fizz_bazz(i: u32) -> &'static str {
+  return if i % 15 == 0 {
+    "fizzbuzz"
   } else if i % 5 == 0 {
-    return "buzz";
+    "buzz"
   } else if i % 3 == 0 {
-    return "fizz";
+    "fizz"
   } else {
-    return "";
-  }
+    ""
+  };
 }
 
 #[test]
 fn fizz_bazz_test() {
-  assert_eq!(print_fizz_bazz(1), "");
-  assert_eq!(print_fizz_bazz(3), "fizz");
-  assert_eq!(print_fizz_bazz(4), "");
-  assert_eq!(print_fizz_bazz(5), "buzz");
-  assert_eq!(print_fizz_bazz(15), "fizzbuzz");
+  assert_eq!(return_fizz_bazz(1), "");
+  assert_eq!(return_fizz_bazz(3), "fizz");
+  assert_eq!(return_fizz_bazz(4), "");
+  assert_eq!(return_fizz_bazz(5), "buzz");
+  assert_eq!(return_fizz_bazz(15), "fizzbuzz");
 }
