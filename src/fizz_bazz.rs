6931e932dfaa320d29f7a3fe6f0cf052ec9f6be7
koji koyama <k.koyama37@gmail.com>
2021-08-10 00:17:45 +0900

    refactor: fizzbuzz判定を関数として分離、テスト設定。

--- src/fizz_bazz.rs
+++ src/fizz_bazz.rs
@@ -1,18 +1,26 @@
 pub fn run() {
   for i in 1..100 {
-    if i % 15 == 0 {
-      println!("{} fizzbuzz", i)
-    }
-    if i % 5 == 0 {
-      println!("{} buzz", i)
-    }
-    if i % 3 == 0 {
-      println!("{} fizz", i)
-    }
+    println!("{} {}", i, print_fizz_bazz(i));
+  }
+}
+
+fn print_fizz_bazz(i: u32) -> &'static str {
+  if i % 15 == 0 {
+    return "fizzbuzz";
+  } else if i % 5 == 0 {
+    return "buzz";
+  } else if i % 3 == 0 {
+    return "fizz";
+  } else {
+    return "";
   }
 }
 
 #[test]
 fn fizz_bazz_test() {
-  assert_eq!(run(2), 4);
+  assert_eq!(print_fizz_bazz(1), "");
+  assert_eq!(print_fizz_bazz(3), "fizz");
+  assert_eq!(print_fizz_bazz(4), "");
+  assert_eq!(print_fizz_bazz(5), "buzz");
+  assert_eq!(print_fizz_bazz(15), "fizzbuzz");
 }
